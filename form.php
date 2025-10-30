5e46bf159b5f18622585c23db9f10b7d69152ecd
ASM SAHLAN <sahlanmuhammad798@gmail.com>
2025-10-30 15:22:01 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+    <title> form handling </title>
+ </head>
+
+<body>
+<form method="POST" action="index.php" >
+ Username:<input type="text" name="username" ><br>
+ Password:<input type="password" name="password"><br>
+ <input type="submit" value="click" name="submit">
+</form>
+</body>
+</html>
